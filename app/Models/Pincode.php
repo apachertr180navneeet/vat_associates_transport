86eec641f947069bac20c48dc7a59c826a41e079
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-12-20 13:04:20 +0530

    city state pincode migaration

---
+++ app/Models/Pincode.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pincode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'pincode',
+        'city_id',
+        'state_id',
+        'country_id',
+    ];
+}
