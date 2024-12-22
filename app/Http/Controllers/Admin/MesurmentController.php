edf89e9114c8d81f5278e03c4715e9ee392870cf
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-12-22 20:56:55 +0530

    mesument master Done

---
+++ app/Http/Controllers/Admin/MesurmentController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\{
+    User,
+    Mesurment
+};
+use Mail, DB, Hash, Validator, Session, File, Exception, Redirect, Auth;
+use Illuminate\Validation\Rule;
+
+class MesurmentController extends Controller
+{
+    /**
+     * Display the User index page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+
+        $compId = $user->firm_id;
+        // Pass the company and comId to the view
+        return view('admin.mesurment.index');
+    }
+
+    /**
+     * Fetch all companies and return as JSON.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getall(Request $request)
+    {
+        $user = Auth::user();
+
+        $compId = $user->firm_id;
+
+        $branchs = Mesurment::where('firm_id',$compId)->get();
+
+        return response()->json(['data' => $branchs]);
+    }
+
+    /**
+     * Update the status of a User.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function status(Request $request)
+    {
+        try {
+            $User = Mesurment::findOrFail($request->userId);
+            $User->status = $request->status;
+            $User->save();
+
+            return response()->json(['success' => true]);
+        } catch (Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()]);
+        }
+    }
+
+    /**
+     * Delete a User by its ID.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        try {
+            Mesurment::where('id', $id)->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Branch deleted successfully',
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        // Validation rules
+        $rules = [
+            'name' => 'required|string',
+        ];
+
+        // Validate the request data
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $user = Auth::user();
+
+        $compId = $user->firm_id;
+        // Save the User data
+        $dataUser = [
+            'name' => $request->name,
+            'firm_id' =>  $compId
+        ];
+        Mesurment::create($dataUser);
+        return response()->json([
+            'success' => true,
+            'message' => 'Branch saved successfully!',
+        ]);
+    }
+
+    // Fetch user data
+    public function get($id)
+    {
+        $user = Mesurment::find($id);
+        return response()->json($user);
+    }
+
+    // Update user data
+    public function update(Request $request)
+    {
+        $request->validate([
+           'name' => 'required|string',
+        ]);
+
+        $user = Mesurment::find($request->id);
+        if ($user) {
+            $user->update($request->all());
+            return response()->json(['success' => true , 'message' => 'Branch Update Successfully']);
+        }
+
+        return response()->json(['success' => false, 'message' => 'Branch not found']);
+    }
+}
--- resources/views/admin/layouts/elements/left_sidebar.blade.php
+++ resources/views/admin/layouts/elements/left_sidebar.blade.php
@@ -20,7 +20,7 @@
 			</a>
 		</li>
 
-		<li class="menu-item {{ request()->routeIs('admin.location.*') || request()->routeIs('admin.branch.*') || request()->routeIs('admin.bank.*') || request()->routeIs('admin.account.*')|| request()->routeIs('admin.area.*')|| request()->routeIs('admin.dealer.*') ? 'active open' : '' }}">
+		<li class="menu-item {{ request()->routeIs('admin.location.*') || request()->routeIs('admin.branch.*') || request()->routeIs('admin.bank.*') || request()->routeIs('admin.account.*')|| request()->routeIs('admin.area.*')|| request()->routeIs('admin.dealer.*') || request()->routeIs('admin.mesurment.*') ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div class="text-truncate" data-i18n="Master">Master</div>
@@ -33,6 +33,7 @@
 					['route' => 'admin.account.index', 'text' => 'Account'],
 					['route' => 'admin.area.index', 'text' => 'Area'],
 					['route' => 'admin.dealer.index', 'text' => 'Dealer'],
+					['route' => 'admin.mesurment.index', 'text' => 'Mesurment'],
                 ] as $mastermenu)
                     <li class="menu-item {{ request()->routeIs($mastermenu['route']) ? 'active' : '' }}">
                         <a href="{{ route($mastermenu['route']) }}" class="menu-link">
