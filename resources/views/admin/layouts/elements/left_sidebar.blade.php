32f9398970003641edce0e937923ff3c2b6adc6c
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-12-23 10:29:48 +0530

    Method Master Done

--- resources/views/admin/layouts/elements/left_sidebar.blade.php
+++ resources/views/admin/layouts/elements/left_sidebar.blade.php
@@ -20,7 +20,7 @@
 			</a>
 		</li>
 
-		<li class="menu-item {{ request()->routeIs('admin.location.*') || request()->routeIs('admin.branch.*') || request()->routeIs('admin.bank.*') || request()->routeIs('admin.account.*')|| request()->routeIs('admin.area.*')|| request()->routeIs('admin.dealer.*') || request()->routeIs('admin.mesurment.*')|| request()->routeIs('admin.groups.*') ? 'active open' : '' }}">
+		<li class="menu-item {{ request()->routeIs('admin.location.*') || request()->routeIs('admin.branch.*') || request()->routeIs('admin.bank.*') || request()->routeIs('admin.account.*')|| request()->routeIs('admin.area.*')|| request()->routeIs('admin.dealer.*') || request()->routeIs('admin.mesurment.*')|| request()->routeIs('admin.groups.*') || request()->routeIs('admin.method.*') ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div class="text-truncate" data-i18n="Master">Master</div>
@@ -35,6 +35,7 @@
 					['route' => 'admin.dealer.index', 'text' => 'Dealer'],
 					['route' => 'admin.mesurment.index', 'text' => 'Mesurment'],
 					['route' => 'admin.groups.index', 'text' => 'Group'],
+					['route' => 'admin.method.index', 'text' => 'Method'],
                 ] as $mastermenu)
                     <li class="menu-item {{ request()->routeIs($mastermenu['route']) ? 'active' : '' }}">
                         <a href="{{ route($mastermenu['route']) }}" class="menu-link">
